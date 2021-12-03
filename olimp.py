81901f27b818c6658653f34f863ec29ac8d8db34
Kadzu-Aris <rust_ice@rambler.ru>
2021-12-03 12:16:23 +0300

    rhfhg

--- olimp.py
+++ olimp.py
@@ -3,7 +3,7 @@ def fill_set():
     str = input()
     while (str!=""):
         _str = str.split()
-        _set[_str[0]] = int(_str[2])
+        _set[_str[0]+ ' ' + _str[1]] = int(_str[2])
         str = input()
     return _set
 
@@ -13,6 +13,7 @@ phys = dict()
 
 count = int(input())
 _list_dict = [0]*count
-for i in range(count):
-    _list_dict.append(fill_set())
+for i in range(0,count):
     _list_dict[i] = fill_set()
+for k in _list_dict:
+    print(k)
